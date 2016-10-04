7c1dcba061254ce3edd0a618084579128041969a
bertonnipaz <bertonnipaz@gmail.com>
2016-10-04 17:41:57 -0300

    Inclusão de função PHP para evitar SQL Injection

--- php/validar_login.php
+++ php/validar_login.php
@@ -1,29 +1,34 @@
 <?php
-include_once 'cabecalho.php';
-
+require_once 'cabecalho.php';
 connectDataBase();
+
 $usuario = $_POST['usuario'];
 $senha = md5($_POST['senha']);
 
-$sql = "SELECT fun_usuario, fun_senha FROM funcionarios WHERE fun_usuario = '$usuario' AND fun_senha = '$senha'";
+$usuario = preg_replace('/[^[:alnum:]_]/', '',$usuario);
+$senha = preg_replace('/[^[:alnum:]_]/', '',$senha);
+
+$sql = "SELECT fun_usuario, fun_senha, fun_role FROM funcionarios WHERE fun_usuario = '$usuario' AND fun_senha = '$senha'";
 $result = mysqli_query($connection, $sql);
 $rows = mysqli_num_rows($result);
+$result = mysqli_fetch_assoc($result);
+$role = $result['fun_role'];
 
 if($rows != 0) {
     unset($_SESSION['login_incorreto']);
     if(!isset($_SESSION['usuario'])) {
         $_SESSION['usuario'] = $usuario;
+        if($role == "admin") {
+          $_SESSION['admin'] = "admin";
+        }
 
-        header("location: cadastro_pacientes.php");
+        header("location: home.php");
     }
 } else {
     if(!isset($_SESSION['login_incorreto'])) {
         $_SESSION['login_incorreto'] = true;
     }
     header("location: login.php");
-          /*echo "<script>
-              alert('Usuário ou senha incorretos!!');
-          </script>";*/
 }
 
 ?>
@@ -33,9 +38,9 @@ if($rows != 0) {
     </div> <!-- /container theme-showcase -->
 </div> <!-- /container -->
 
-
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 <!-- <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script> -->
 <?php
-include_once 'rodape.php';
+disconnectDataBase();
+require_once 'rodape.php';
 ?>
