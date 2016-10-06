415f06e89819c161dd4a5000c1138ddb6b0a5539
bertonnipaz <bertonnipaz@gmail.com>
2016-10-06 19:42:53 -0300

    Inclusão da Mensagem "Não Cadastrado" na coluna 'pac_telefone_2' para quando não for cadastrado o segundo telefone(que não é obrigatório)

--- php/validar_pac.php
+++ php/validar_pac.php
@@ -9,10 +9,10 @@ $cpf = $_POST['cpf'];
 $email = $_POST['email'];
 $nasc = $_POST['nasc'];
 $fone1 = $_POST['fone1'];
-if(isset($_POST['fone2'])){
+if(isset($_POST['fone2']) && $_POST['fone2'] != ""){
 	$fone2 = $_POST['fone2'];
 } else {
-	$fone2 = 1;
+	$fone2 = "Não Cadastrado";
 }
 
 $sql = "INSERT INTO `pacientes` (`pac_nome`, `pac_endereco`, `pac_rg`, `pac_cpf`, `pac_email`, `pac_data_nasc`, `pac_telefone_1`, `pac_telefone_2`) VALUES ('$nome', '$endereco', '$rg', '$cpf', '$email', '$nasc', '$fone1', '$fone2')";
@@ -20,8 +20,9 @@ $sql = "INSERT INTO `pacientes` (`pac_nome`, `pac_endereco`, `pac_rg`, `pac_cpf`
 if(mysqli_query($connection, $sql)) {
 	echo "<div class='container marketing'>
 			<div class='container theme-showcase' role='main'>
-				<h4>Paciente cadastrado com sucesso!!</h4><br>
-				<a href='cadastro_pacientes.php'>Voltar à página de cadastro</a>
+				<h3>Paciente cadastrado com sucesso!!</h3><br>
+				<a href='cadastro_pacientes.php'>Voltar à página de cadastro</a><br>
+				<a href='home.php'>Voltar à página principal</a>
 		  	</div>
 		  </div>
 	";
