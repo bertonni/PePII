b06fdbb551a8aacea98dca392779fd2614d1a5e2
bertonnipaz <bertonnipaz@gmail.com>
2016-10-07 17:24:10 -0300

    Inclusão da Tabela de Agendamento de consultas

---
+++ sql/sistema_de_cadastro_-_06-10-16.sql
@@ -0,0 +1,150 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 07, 2016 at 02:46 PM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 7.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sistema_de_cadastro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `agendamentos`
+--
+
+CREATE TABLE `agendamentos` (
+  `agd_id` int(11) NOT NULL,
+  `agd_fun_id` int(11) NOT NULL,
+  `agd_pac_id` int(11) NOT NULL,
+  `agd_data` date NOT NULL,
+  `agd_hora` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `funcionarios`
+--
+
+CREATE TABLE `funcionarios` (
+  `fun_id` int(11) NOT NULL,
+  `fun_nome` varchar(100) NOT NULL,
+  `fun_data_nasc` date NOT NULL,
+  `fun_usuario` varchar(20) NOT NULL,
+  `fun_senha` varchar(256) NOT NULL,
+  `fun_role` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `funcionarios`
+--
+
+INSERT INTO `funcionarios` (`fun_id`, `fun_nome`, `fun_data_nasc`, `fun_usuario`, `fun_senha`, `fun_role`) VALUES
+(1, 'Bertonni Thiago de Souza Paz', '1986-11-08', 'Bertonni', '094f0c783c40b68a06935a0cb5755862', 'admin'),
+(2, 'Mateus Nascimento', '1993-04-25', 'Mateus', 'e42b6a82864b7060c447ecebd62518a3', 'funcionario');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pacientes`
+--
+
+CREATE TABLE `pacientes` (
+  `pac_id` int(11) NOT NULL,
+  `pac_nome` varchar(100) NOT NULL,
+  `pac_endereco` varchar(200) NOT NULL,
+  `pac_rg` varchar(10) NOT NULL,
+  `pac_cpf` varchar(14) NOT NULL,
+  `pac_email` varchar(50) NOT NULL,
+  `pac_data_nasc` date NOT NULL,
+  `pac_telefone_1` varchar(15) NOT NULL,
+  `pac_telefone_2` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pacientes`
+--
+
+INSERT INTO `pacientes` (`pac_id`, `pac_nome`, `pac_endereco`, `pac_rg`, `pac_cpf`, `pac_email`, `pac_data_nasc`, `pac_telefone_1`, `pac_telefone_2`) VALUES
+(4, 'Gleison Ytalo', 'rua alguma coisa', '7.654.321', '214.748.364-71', 'gleibsonytalo12@gmail.com', '2016-08-16', '(81) 99999-9999', '(81) 98888-8888'),
+(5, 'Ranieri ValenÃ§a', 'rua da programaÃ§Ã£o', '7.829.593', '214.748.364-77', 'peladadoamigao2005@gmail.com', '2016-08-09', '(81) 2222-2222', 'NÃ£o Cadastrado'),
+(6, 'Bertonni Thiago de Souza Paz', 'rua de teste', '22.333.222', '214.748.364-70', 'bertonnipaz@gmail.com', '2016-08-09', '(81) 98733-0562', 'NÃ£o Cadastrado'),
+(7, 'Gabriella StÃ©phanie Rocha Rodrigues Paz', 'rua bla bla bla', '7.789.655', '221.365.987-21', 'gabriella@email.com', '1991-06-11', '(81) 99888-7744', 'NÃ£o Cadastrado'),
+(8, 'Mateus Nascimento', 'Rua de teste, 43', '7.722.512', '123.445.967-22', 'mateus@email.com', '1990-07-10', '(81) 98522-4472', '(81) 3545-2255'),
+(9, 'Flaviana Varino', 'Rua de Flaviana, 57', '1.234.520', '425.332.410-55', 'flaviana@email.com', '1989-04-25', '(81) 98811-2235', 'NÃ£o Cadastrado'),
+(10, 'Adauto Pereira', 'Rua tal, 34', '4.332.101', '122.302.014-27', 'adauto@email.com', '1997-04-23', '(81) 97765-5433', '(81) 3542-5343'),
+(11, 'Sergio Bandeira', 'Rua de Sergio, 123', '1.234.770', '321.002.951-24', 'sergio@email.com', '1990-04-19', '(81) 98877-4419', '(81) 3542-1414'),
+(12, 'JoÃ£o Paulo Pereira', 'Rua de JoÃ£o Paulo, 44', '22.014.71', '200.320.014-42', 'joaopaulo@email.com', '1997-02-10', '(81) 99521-1011', 'NÃ£o Cadastrado'),
+(13, 'Allan Diego Lima', 'Rua do Algoritmo, 100', '71.202.00', '201.101.023-65', 'allanlima@email.com', '1982-10-25', '(81) 99550-0332', 'NÃ£o Cadastrado');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `agendamentos`
+--
+ALTER TABLE `agendamentos`
+  ADD PRIMARY KEY (`agd_id`),
+  ADD KEY `agd_fun_fk` (`agd_fun_id`),
+  ADD KEY `agd_pac_fk` (`agd_pac_id`);
+
+--
+-- Indexes for table `funcionarios`
+--
+ALTER TABLE `funcionarios`
+  ADD PRIMARY KEY (`fun_id`);
+
+--
+-- Indexes for table `pacientes`
+--
+ALTER TABLE `pacientes`
+  ADD PRIMARY KEY (`pac_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `agendamentos`
+--
+ALTER TABLE `agendamentos`
+  MODIFY `agd_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `funcionarios`
+--
+ALTER TABLE `funcionarios`
+  MODIFY `fun_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `pacientes`
+--
+ALTER TABLE `pacientes`
+  MODIFY `pac_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `agendamentos`
+--
+ALTER TABLE `agendamentos`
+  ADD CONSTRAINT `agd_fun_fk` FOREIGN KEY (`agd_fun_id`) REFERENCES `funcionarios` (`fun_id`),
+  ADD CONSTRAINT `agd_pac_fk` FOREIGN KEY (`agd_pac_id`) REFERENCES `pacientes` (`pac_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
