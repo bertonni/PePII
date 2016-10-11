ad978d315b29e4691a281eaac4f9da8010032ab1
bertonnipaz <bertonnipaz@gmail.com>
2016-10-11 08:51:11 -0300

    Alteração do layout da página home.php, sql atualizado

--- php/home.php
+++ php/home.php
@@ -5,18 +5,36 @@ require_once 'cabecalho.php';
     .ancor:hover {
         text-decoration: none;
     }
+    .add_user {
+        float: left;
+        margin-left: 22%;
+        margin-top: 9%;
+    }
+    .search_user {
+        margin-left: 52%;
+        margin-top: 10.93%;
+    }
+    h3 {
+        margin-left: 6%;
+    }
 </style>
 <title>Home</title>
 
 <div class="container">
 	<div class="container theme-showcase" role="main">
 		<div class="row">
-            <a href="cadastro_pacientes.php" class="ancor" title="Cadastrar Paciente">
-                <img src="../imagens/adcusuario.png" alt="Adicionar Usuário">
-            </a>
-            <a href="busca.php" class="ancor" title="Buscar Paciente">
-                <img src="../imagens/busca.png" alt="Adicionar Usuário">
-            </a>
+            <div class="add_user">
+                <h3>Cadastrar Paciente</h3><br>
+                <a href="cadastro_pacientes.php" class="ancor" title="Cadastrar Paciente">
+                    <img src="../imagens/adcusuario.png" alt="Adicionar Usuário">
+                </a>
+            </div>
+            <div class="search_user">
+                <h3>Procurar Paciente</h3><br>
+                <a href="busca.php" class="ancor" title="Buscar Paciente">
+                    <img src="../imagens/busca.png" alt="Adicionar Usuário">
+                </a>
+            </div>
 		</div>
 	</div> <!-- /container theme-showcase -->
 </div> <!-- /container -->
