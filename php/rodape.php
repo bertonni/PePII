1e6e2fa733597c39df5c21868c3d7e8c09f12f9b
bertonnipaz <bertonnipaz@gmail.com>
2016-11-25 21:25:51 -0300

    Inclusão do script do introJs

--- php/rodape.php
+++ php/rodape.php
@@ -23,6 +23,7 @@
         });
       };
     </script>
+    <script type="text/javascript" src="../js/intro.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
     <script src="../js/bootstrap.min.js"></script>
     <!-- <script src="../js/jquery.maskedinput.js"></script> -->
