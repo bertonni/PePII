475082652290393ad8b4eff241c655aa8d7b35d0
bertonnipaz <bertonnipaz@gmail.com>
2016-11-26 22:32:20 -0300

    Inclusão de link para recuperar senha e função de identificação do capsLock ativo

--- php/login.php
+++ php/login.php
@@ -13,7 +13,8 @@ if(isLogged()) {
 			<label for="user" class="sr-only">Usuário</label>
 			<input type="text" id="user" class="form-control user" name="usuario" placeholder="Usuário" required autofocus>
 			<label for="password" class="sr-only">Senha</label>
-			<input type="password" id="password" class="form-control" name="senha" placeholder="Senha" required>
+			<input type="password" id="password" class="form-control" name="senha" placeholder="Senha" required onkeypress="capsLock(event)">
+			<div id="caps" style="visibility:hidden">&#9708; Caps Lock ativado</div>
 			<button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
 			<?php
 				// Checa se a variável de sessão "$_SESSION['login_incorreto']" está setada e se é verdadeira (Se o usuário errou o login)
@@ -22,10 +23,22 @@ if(isLogged()) {
 					echo '<spam class="erro">*** Usuário ou senha incorretos ***</spam>';
 				}
 			?>
+			<div class="col col-md-12 recovery">
+				<a href="recuperar_senha.php">Recuperar senha</a>
+			</div>
 		</form>
 	</div> <!-- /container theme-showcase -->
 </div> <!-- /container -->
-
+<script>
+function capsLock(e){
+ kc = e.keyCode?e.keyCode:e.which;
+ sk = e.shiftKey?e.shiftKey:((kc == 16)?true:false);
+ if(((kc >= 65 && kc <= 90) && !sk)||((kc >= 97 && kc <= 122) && sk))
+  document.getElementById('caps').style.visibility = 'visible';
+ else
+  document.getElementById('caps').style.visibility = 'hidden';
+}
+</script>
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 <!-- <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script> -->
 <?php
